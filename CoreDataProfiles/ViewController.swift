60e3bb864067c94c0f99e0f680312bf402e9ef66
Bagmet <m.bagmet@gmail.com>
2022-05-28 13:51:47 +0200

    Initial Commit

---
+++ CoreDataProfiles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProfiles
+//
+//  Created by Михаил Багмет on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
